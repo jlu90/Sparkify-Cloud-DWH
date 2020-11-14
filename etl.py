26ce9e99cca2138f0feeeacd8e29f009ef541b03
Jocelyn Lutes <jocelyn.lutes@gmail.com>
2020-11-13 20:07:43 -0500

    fixed sql queries and S3 songs pathway

--- etl.py
+++ etl.py
@@ -5,25 +5,32 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 def load_staging_tables(cur, conn):
     for query in copy_table_queries:
+        print(query)
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
     for query in insert_table_queries:
+        print(query)
         cur.execute(query)
         conn.commit()
 
 
 def main():
-    config = configparser.ConfigParser()
+    config = configparser.ConfigParser(interpolation=None)
     config.read('dwh.cfg')
 
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
-    
+
+    print('Loading staging tables.')
     load_staging_tables(cur, conn)
+    print('Staging tables complete.')
+    
+    print('Inserting data into tables.')
     insert_tables(cur, conn)
+    print('Data insert complete.')
 
     conn.close()
 
--- sql_queries.py
+++ sql_queries.py
@@ -2,7 +2,7 @@ import configparser
 
 
 # CONFIG
-config = configparser.ConfigParser()
+config = configparser.ConfigParser(interpolation=None)
 config.read('dwh.cfg')
 IAM_ROLE = config.get('IAM_ROLE', 'ARN')
 LOG_DATA = config.get('S3', 'LOG_DATA')
@@ -11,13 +11,13 @@ SONG_DATA = config.get('S3', 'SONG_DATA')
 
 # DROP TABLES
 
-staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
-staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
-songplay_table_drop = "DROP TABLE IF EXISTS songplay"
-user_table_drop = "DROP TABLE IF EXISTS users"
-song_table_drop = "DROP TABLE IF EXISTS songs"
-artist_table_drop = "DROP TABLE IF EXISTS artists"
-time_table_drop = "DROP TABLE IF EXISTS time"
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events CASCADE"
+staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs CASCADE"
+songplay_table_drop = "DROP TABLE IF EXISTS songplay CASCADE"
+user_table_drop = "DROP TABLE IF EXISTS users CASCADE"
+song_table_drop = "DROP TABLE IF EXISTS songs CASCADE"
+artist_table_drop = "DROP TABLE IF EXISTS artists CASCADE"
+time_table_drop = "DROP TABLE IF EXISTS time CASCADE"
 
 # CREATE TABLES
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS staging_events (
     sessionId INT,
     song VARCHAR,
     status INT,
-    ts DOUBLE PRECISION,
+    ts BIGINT,
     userAgent TEXT,
     userId INT
 )
@@ -129,59 +129,51 @@ staging_songs_copy = ("""
 COPY staging_songs FROM {}
 credentials 'aws_iam_role={}'
 region 'us-west-2'
-json 'auto'
+json 'auto' TRUNCATECOLUMNS
 """).format(SONG_DATA, IAM_ROLE)
 
 # FINAL TABLES
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
-VALUES(
-    SELECT DISTINCT se.userId, se.firstName, se.lastName, se.gender, se.level
-    FROM staging_events se
-    WHERE se.userId IS NOT NULL
-)
+SELECT DISTINCT se.userId, se.firstName, se.lastName, se.gender, se.level
+FROM staging_events se
+WHERE se.userId IS NOT NULL
+
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
-VALUES (
-    SELECT DISTINCT s.song_id, s.title, s.artist_id, s.year, s.duration
-    FROM staging_songs s
-    WHERE s.song_id IS NOT NULL
-)
+SELECT DISTINCT s.song_id, s.title, s.artist_id, s.year, s.duration
+FROM staging_songs s
+WHERE s.song_id IS NOT NULL
+
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
-VALUES (
-    SELECT DISTINCT s.artist_id, s.artist_name, s.artist_location, s.artist_latitude, s.artist_longitude
-    FROM staging_songs s
-    WHERE s.artist_id IS NOT NULL
-)
+SELECT DISTINCT s.artist_id, s.artist_name, s.artist_location, s.artist_latitude, s.artist_longitude
+FROM staging_songs s
+WHERE s.artist_id IS NOT NULL
+
 """)
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-VALUES (
-    SELECT DISTINCT s.start_time, EXTRACT(HOUR FROM s.start_time), EXTRACT(DAY FROM s.start_time), EXTRACT(WEEK FROM s.start_time), EXTRACT(MONTH FROM s.start_time), EXTRACT(YEAR FROM s.start_time), EXTRACT(WEEKDAY FROM s.start_time)
-    FROM songplays s
-)
+SELECT DISTINCT s.start_time, EXTRACT(HOUR FROM s.start_time), EXTRACT(DAY FROM s.start_time), EXTRACT(WEEK FROM s.start_time), EXTRACT(MONTH FROM s.start_time), EXTRACT(YEAR FROM s.start_time), EXTRACT(WEEKDAY FROM s.start_time)
+FROM songplays s
 """)
 
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-VALUES (
-    SELECT DISTINCT timestamp 'epoch' + se.ts/1000 * interval '1 second' as start_time, se.userId, se.level, s.song_id, a.artist_id, se.sessionId, se.location, se.userAgent
-    FROM staging_events se 
-        LEFT JOIN songs s ON s.title = se.song
-        LEFT JOIN artists a ON a.name = se.artist
-    WHERE s.artist_id = a.artist_id)
+SELECT TIMESTAMP 'epoch' + se.ts/1000 * interval '1 second' as start_time, se.userId, se.level, ss.song_id, ss.artist_id, se.sessionId, se.location, se.userAgent
+FROM staging_events se LEFT JOIN staging_songs ss ON (se.song = ss.title AND se.artist = ss.artist_name)
+WHERE se.page = 'NextSong'
 """)
 
 # QUERY LISTS
 
 create_table_queries = [staging_events_table_create, staging_songs_table_create, user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [staging_events_table_drop, staging_songs_table_drop, songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
-#copy_table_queries = [staging_events_copy, staging_songs_copy]
-#insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
+copy_table_queries = [staging_events_copy, staging_songs_copy]
+insert_table_queries = [songplay_table_insert, user_table_insert, song_table_insert, artist_table_insert, time_table_insert]
