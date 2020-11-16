19956c77c8075cf134296dba8d88a9e4faf238d4
Jocelyn Lutes <jocelyn.lutes@gmail.com>
2020-11-16 09:17:31 -0500

    updated sql queries

--- sql_queries.py
+++ sql_queries.py
@@ -3,7 +3,7 @@ import configparser
 
 # CONFIG INFORMATION
 config = configparser.ConfigParser(interpolation=None)
-config.read('dwh.cfg')
+config.read('dwh_private.cfg')
 IAM_ROLE = config.get('IAM_ROLE', 'ARN')
 LOG_DATA = config.get('S3', 'LOG_DATA')
 LOG_JSONPATH = config.get('S3', 'LOG_JSONPATH')
@@ -23,20 +23,20 @@ time_table_drop = "DROP TABLE IF EXISTS time CASCADE"
 
 staging_events_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_events (
-    artist VARCHAR(255),
-    auth VARCHAR(20),
-    firstName VARCHAR(100),
+    artist VARCHAR,
+    auth VARCHAR,
+    firstName VARCHAR,
     gender CHARACTER,
     itemInSession INT,
-    lastName VARCHAR(100),
+    lastName VARCHAR,
     length FLOAT,
-    level VARCHAR(20),
-    location VARCHAR(255),
-    method VARCHAR(10),
-    page VARCHAR(20),
-    registration FLOAT,
+    level VARCHAR,
+    location VARCHAR,
+    method VARCHAR,
+    page VARCHAR,
+    registration VARCHAR,
     sessionId INT,
-    song VARCHAR(255),
+    song VARCHAR,
     status INT,
     ts BIGINT,
     userAgent TEXT,
@@ -47,13 +47,13 @@ CREATE TABLE IF NOT EXISTS staging_events (
 staging_songs_table_create = ("""
 CREATE TABLE IF NOT EXISTS staging_songs (
     num_songs INT,
-    artist_id VARCHAR(20),
+    artist_id VARCHAR,
     artist_latitude FLOAT,
     artist_longitude FLOAT,
-    artist_location VARCHAR(255),
-    artist_name VARCHAR(255),
-    song_id VARCHAR(20),
-    title VARCHAR(255), 
+    artist_location VARCHAR,
+    artist_name VARCHAR,
+    song_id VARCHAR,
+    title VARCHAR, 
     duration FLOAT,
     year INT
 )
@@ -64,30 +64,30 @@ CREATE TABLE IF NOT EXISTS songplays (
     songplay_id INT IDENTITY (0,1) PRIMARY KEY,
     start_time TIMESTAMP NOT NULL REFERENCES time(start_time),
     user_id INT NOT NULL REFERENCES users(user_id),
-    level VARCHAR(20),
-    song_id VARCHAR(20) NOT NULL REFERENCES songs(song_id),
-    artist_id VARCHAR(20) NOT NULL REFERENCES artists(artist_id),
+    level VARCHAR ,
+    song_id VARCHAR NOT NULL REFERENCES songs(song_id),
+    artist_id VARCHAR NOT NULL REFERENCES artists(artist_id),
     session_id INT,
-    location VARCHAR(255),
+    location VARCHAR,
     user_agent TEXT
 )
 """)
 
 user_table_create = ("""
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT PRIMARY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR,
+    last_name VARCHAR,
     gender CHARACTER,
-    level VARCHAR(20)
+    level VARCHAR
 )
 """)
 
 song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
-    song_id VARCHAR(20) PRIMARY KEY,
-    title VARCHAR(255),
-    artist_id VARCHAR(20) NOT NULL REFERENCES artists(artist_id),
+    song_id VARCHAR PRIMARY KEY,
+    title VARCHAR,
+    artist_id VARCHAR NOT NULL REFERENCES artists(artist_id),
     year INT,
     duration FLOAT
 )
@@ -95,9 +95,9 @@ CREATE TABLE IF NOT EXISTS songs (
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
-    artist_id VARCHAR(20) PRIMARY KEY,
-    name VARCHAR(255),
-    location VARCHAR(255),
+    artist_id VARCHAR PRIMARY KEY,
+    name VARCHAR,
+    location VARCHAR,
     latitude FLOAT,
     longitude FLOAT
 )
@@ -136,15 +136,25 @@ json 'auto' TRUNCATECOLUMNS
 
 user_table_insert = ("""
 INSERT INTO users (user_id, first_name, last_name, gender, level)
-SELECT DISTINCT se.userId, se.firstName, se.lastName, se.gender, se.level
+SELECT DISTINCT 
+    se.userId, 
+    se.firstName, 
+    se.lastName, 
+    se.gender, 
+    se.level
 FROM staging_events se
-WHERE se.userId IS NOT NULL
+WHERE se.userId IS NOT NULL AND se.page = 'NextSong'
 
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
-SELECT DISTINCT s.song_id, s.title, s.artist_id, s.year, s.duration
+SELECT DISTINCT 
+    s.song_id, 
+    s.title, 
+    s.artist_id, 
+    s.year, 
+    s.duration
 FROM staging_songs s
 WHERE s.song_id IS NOT NULL
 
@@ -152,7 +162,12 @@ WHERE s.song_id IS NOT NULL
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, name, location, latitude, longitude)
-SELECT DISTINCT s.artist_id, s.artist_name, s.artist_location, s.artist_latitude, s.artist_longitude
+SELECT DISTINCT 
+    s.artist_id, 
+    s.artist_name, 
+    s.artist_location, 
+    s.artist_latitude, 
+    s.artist_longitude
 FROM staging_songs s
 WHERE s.artist_id IS NOT NULL
 
@@ -160,14 +175,31 @@ WHERE s.artist_id IS NOT NULL
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-SELECT DISTINCT s.start_time, EXTRACT(HOUR FROM s.start_time), EXTRACT(DAY FROM s.start_time), EXTRACT(WEEK FROM s.start_time), EXTRACT(MONTH FROM s.start_time), EXTRACT(YEAR FROM s.start_time), EXTRACT(WEEKDAY FROM s.start_time)
-FROM songplays s
+SELECT DISTINCT 
+    TIMESTAMP 'epoch' + (se.ts/1000) * INTERVAL '1 Second ' AS start_time,
+    EXTRACT(HOUR FROM start_time), 
+    EXTRACT(DAY FROM start_time), 
+    EXTRACT(WEEK FROM start_time), 
+    EXTRACT(MONTH FROM start_time), 
+    EXTRACT(YEAR FROM start_time), 
+    EXTRACT(WEEKDAY FROM start_time)
+FROM staging_events se
+WHERE se.page  = 'NextSong'
 """)
 
 songplay_table_insert = ("""
 INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT TIMESTAMP 'epoch' + se.ts/1000 * interval '1 second' as start_time, se.userId, se.level, ss.song_id, ss.artist_id, se.sessionId, se.location, se.userAgent
-FROM staging_events se LEFT JOIN staging_songs ss ON (se.song = ss.title AND se.artist = ss.artist_name)
+SELECT DISTINCT 
+    TIMESTAMP 'epoch' + se.ts/1000 * interval '1 second' as start_time, 
+    se.userId, 
+    se.level, 
+    ss.song_id, 
+    ss.artist_id, 
+    se.sessionId, 
+    se.location, 
+    se.userAgent
+FROM staging_events se 
+    JOIN staging_songs ss ON (se.song = ss.title AND se.artist = ss.artist_name)
 WHERE se.page = 'NextSong'
 """)
 
